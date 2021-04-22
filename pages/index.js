01d2f477889515ad3bb7b138daa8f960332e0af3
TiagoMendesSilva <tiago_mendessilva@yahoo.com>
2021-04-21 23:17:02 -0300

    Altera index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,12 @@
-import { useState } from 'react';
-
 function Home() {
     return (
         <div>
             <h1>HOME</h1>
-            <h2>Teste</h2>
-            <contador /> 
+          
         </div>
     )
 }
 
-function Contador() {
-    const [contador,setContador] = useState(1);
 
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return(
-        <div>
-            <div>{contador}</div>
-            <button onClick = {adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
 
 export default Home
